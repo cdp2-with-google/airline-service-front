d9b38934a4b11cc32a417e9a60b504c98ba4b85b
Climier-code <ikowhddus@naver.com>
2024-12-01 12:26:56 +0900

    fix: oauth client id

--- src/constants/oauth.ts
+++ src/constants/oauth.ts
@@ -1,1 +1,1 @@
-export const GOOGLE_CLIENT_ID = 'https://965998489425-b14jbag85kh6g0cog4ro3kd5jjf9lpql.apps.googleusercontent.com/';
+export const GOOGLE_CLIENT_ID = '965998489425-b14jbag85kh6g0cog4ro3kd5jjf9lpql.apps.googleusercontent.com';
